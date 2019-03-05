992edaf3ccc218bcaab75a94a8203abf4ad00916
Justus <justus.rho4@gmail.com>
2019-03-05 17:51:00 -0600

    Add files via upload

--- caesar.py
+++ caesar.py
@@ -27,22 +27,48 @@ def rotate_character(char, rot):
     # String of uppercase letters
     upper_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-    # Keep number of shifts done
-    #
-
     # If the character is a letter, shift it by rot
     if char.isalpha():
         # Create variable for char's alphabet position
         char_position = alphabet_position(char)
+
+        # Keep number of shifts done
+        num_shifts = 0
+
+        # Shifted character
+        shifted_char = char
+
         if char.isupper():
-            return upper_letters[char_position + (int(rot) % 26)]
+            while num_shifts < rot:
+                num_shifts += 1
+                if alphabet_position(shifted_char) == 25:
+                    shifted_char = upper_letters[0]
+                else:
+                    shifted_char = upper_letters[alphabet_position(shifted_char) + 1]
+            return shifted_char
         else:
-            return lower_letters[char_position + (int(rot) % 26)]
+            while num_shifts < rot:
+                num_shifts += 1
+                if alphabet_position(shifted_char) == 25:
+                    shifted_char = lower_letters[0]
+                else:
+                    shifted_char = lower_letters[alphabet_position(shifted_char) + 1]
+            return shifted_char
     # If char is not a letter, return it unchanged
     else:
         return char
 
 # Encrypts all letters in text by rot letters
-# Example: 
-#
-# def encrypt(text, rot):
+def encrypt(text, rot):
+    crypted_text = ''
+    for char in text:
+        crypted_text += rotate_character(char, rot)
+    return crypted_text
+
+def main():
+    text = str(input("Type a message:\n"))
+    rotation = int(input("Rotate by:\n"))
+    print(encrypt(text, rotation))
+
+if __name__ == "__main__":
+    main()
